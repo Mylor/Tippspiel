dadb2c5412280d25dacee1c54b3b1becdfc12bfc
Fabian <fabian.doedens@tu-dortmund.de>
2026-05-30 17:05:50 +0200

    Beta 1.1 (Keys verstecken)

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kdswfjqmsaooewznzcgs.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkc3dmanFtc2Fvb2V3em56Y2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MDMzNDIsImV4cCI6MjA5MDE3OTM0Mn0.pjorE2OA5aIv8td8JoahOV5rQFR5-vXZrl0Yeiy-rv8'
+// Hier ziehen wir uns die Werte dynamisch aus den Umgebungsvariablen
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
